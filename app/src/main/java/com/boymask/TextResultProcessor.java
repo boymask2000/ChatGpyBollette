ad9bad55ff04eb2ddaa80a7fdec506c792ee7834
Giovanni Posabella <gposabella@gmail.com>
2026-05-16 20:35:47 +0200

    formattazione testo

--- app/src/main/java/com/boymask/TextResultProcessor.java
+++ app/src/main/java/com/boymask/TextResultProcessor.java
@@ -3,8 +3,7 @@ package com.boymask;
 public class TextResultProcessor {
 
     public static String process( String text ){
-        String addon = "Se hai dei dubbi su qualche termine usato in questa risposta, " + //
-                "puoi fare riferimento al Glossario che trovi nelle opzioni della App, nel menu in alto a destra";
+        String addon = "Se hai dei dubbi su qualche termine usato nella tua bolletta puoi fare riferimento al Glossario che trovi nel menù in alto a destra";
 
         return text+ "\n\n"+addon;
     }
